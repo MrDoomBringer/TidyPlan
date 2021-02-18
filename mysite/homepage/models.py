0bd5d504638c216134dcfeeece500540a8536bdb
Ptran36 <78627295+Ptran36@users.noreply.github.com>
2021-02-18 17:28:53 -0500

    Set up model for User and Task

--- mysite/homepage/models.py
+++ mysite/homepage/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class User(models.Model):		
+    name_text = models.CharField(max_length = 200)
+    def __str__(self):
+        return self.name_text
+
+
+class Task(models.Model):
+    description_text = models.CharField(max_length=200)
+    due_date = models.DateTimeField('Due Date')
+    time_estimate = models.IntegerField(default=0)
+    def __str__(self):
+        return self.description_text
