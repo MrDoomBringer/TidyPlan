6748a63f01263b5427a9cdd5e8387a8e97a60b97
Niruta5 <66476335+Niruta5@users.noreply.github.com>
2021-02-25 15:55:52 -0500

    Merge pull request #11 from Ptran36/Anything
    Set up model for User and Task

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path("<str:name_text>",views.index, name = "index"),
+	path("",views.home,name ="home"),
 ]
