ae28f36fc98186b11221a31662efb174a69c9dea
Niruta5 <66476335+Niruta5@users.noreply.github.com>
2021-02-25 19:54:47 -0500

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('admin/', admin.site.urls),
 	path("<str:name_text>",views.index, name = "index"),
 	path("",views.home,name ="home"),
 ]
