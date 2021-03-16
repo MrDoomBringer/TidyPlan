4f95d03f087372a4bf8cf91d1edeaf9ba6c6207f
MrDoomBringer <mrdoomboyo@gmail.com>
2021-03-15 21:55:19 -0400

    Edit views, models, and relative html

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -6,10 +6,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('faq', views.faq, name='faq'),
-    path('howto', views.howto, name='howto'),
-    path('account', views.account, name='account'),
-    path('calendar', views.CalendarView.as_view(), name='calendar'),
-    path('tos', views.tos, name='tos'),
+    path('faq/', views.faq, name='faq'),
+    path('howto/', views.howto, name='howto'),
+    path('account/', views.account, name='account'),
+    path('calendar/', views.calendar, name='calendar'),
+    path('tos/', views.tos, name='tos'),
     path("create/", views.create, name = "create"),
 ]
--- mysite/pages/views.py
+++ mysite/pages/views.py
@@ -6,32 +6,45 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
-from .models import User, Task
+from .models import User, Task, WebsiteMeta
 
 
 def index(request):
-    template_name = 'pages/home.html'
-    return render(request, template_name)
+	template_name = 'pages/home.html'
+	return render(request, template_name)
 
 def faq(request):
-    return HttpResponse("FAQ")
+	return HttpResponse("FAQ")
 
 def howto(request):
-    return HttpResponse("How To")
+	return HttpResponse("How To")
 
 def account(request):
-    return HttpResponse("Account Page")
+	return HttpResponse("Account Page")
 
-class CalendarView(generic.ListView):
-    template_name = 'pages/tasks.html'
-    context_object_name = 'tsk_list'
+#See tasks.html for related HTML code
+def calendar(request):
+	if (request.method == "POST"):
+		if ('new_task' in request.POST): #If the form that we submitted has the name 'new_task'
+			check_websitemeta()
+			td = ToDoList()
+			td.save()
+			t = Task()
+			t.description_text = f'Task number [{total_tasks_ever_made()}]'
+			t.todolist = td
+			t.task_id=timezone.now()
+			t.save()
+			total_tasks_ever_made(increment=1)
 
-    def get_queryset(self):
-        """Return the published tasks."""
-        return Task.objects.filter(due_date__lte=timezone.now()).order_by('-due_date')
+		if ('delete_task' in request.POST): #If the form that we submitted has the name 'delete_task'
+			id_to_delete = request.POST['deleted_task'] #Get the ID of the task. This is stored in a input tag of type='hidden'
+			Task.objects.filter(task_id=id_to_delete).delete()
+	
+	tsk_list = Task.objects.filter(due_date__lte=timezone.now()).order_by('-due_date')
+	return render(request, 'pages/tasks.html', {'tsk_list': tsk_list})
 
 def tos(request):
-    return HttpResponse("Terms of Service")
+	return HttpResponse("Terms of Service")
 
 def create(response):
 	if response.method == "POST":
@@ -45,3 +58,23 @@ def create(response):
 	else:
 		form = CreateNewList()
 	return render(response, "pages/create.html", {"form": form})
+
+#Check to see if we have a 'WebsiteMeta' object in the database. If not, add one
+def check_websitemeta():
+	#Use objects.all() to get a list of all 'WebsiteMeta' objects (there should only really be one)
+	num_website_metas = len(WebsiteMeta.objects.all())
+	if (num_website_metas == 0):
+		print("No WebsiteMeta object found, creating a new one.")
+		wm = WebsiteMeta()
+		wm.save()
+	elif(num_website_metas != 1):
+		#We should only ever have one max, so if there's more than 1 raise an error
+		raise Exception("More than one WebsiteMeta object found, only 1 should exist at a time. Please let Emmanuel know about this!")
+
+#Function to get or set the total tasks ever made by the website. Uses the 'WebsiteMeta' database entry to keep track of this
+def total_tasks_ever_made(increment = 0):
+	check_websitemeta() #Make sure we have a websitemeta object to work with
+	wm = WebsiteMeta.objects.all()[0] #Get the first (and only) WebsiteMeta object from our database
+	wm.total_tasks_created += increment
+	wm.save()
+	return wm.total_tasks_created
