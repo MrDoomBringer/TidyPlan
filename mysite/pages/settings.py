a66bab95cff810c45652492092d8284a004e29ed
MrDoomBringer <mrdoomboyo@gmail.com>
2021-05-06 11:33:50 -0400

    Implements logging in to test cases

--- mysite/pages/settings.py
+++ mysite/pages/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'kkou)=2cn8d6q=6u31xgm49#y+k$d&qj1k_5)weusax%a+qrmf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testserver']
 
 
 # Application definition
--- mysite/pages/tests.py
+++ mysite/pages/tests.py
@@ -1,13 +1,18 @@
 import datetime
 
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.utils import timezone
 from django.urls import reverse
 from .models import Task, ToDoList, User
 
 class QuestionModelTests(TestCase):
     def test_no_questions(self):
-        response = self.client.get(reverse('calendar'))
+        c = Client()
+        user = User.objects.create_user("test_user", password="test_password")
+        user.save()
+        c.login(username="test_user", password="test_password")
+
+        response = c.get(reverse('calendar'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No tasks are available to view")
         self.assertQuerysetEqual(
